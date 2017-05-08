2c7ee4ad39bdb238be64d67ff59e5942c68cdb30
jason <jk@airsensing.com>
2017-05-08 19:08:09 -0400

    chessboard now accepts webcam stream and tracks the four corners

--- src/chessboard.cpp
+++ src/chessboard.cpp
@@ -11,75 +11,73 @@
 #include "ibvs.h"
 #include "ctime"
 
+#include "opencv2/opencv.hpp"
+#include <iostream>
 
 static const int CBOARD_COL = 5;
 static const int CBOARD_ROW = 4;
 
-using namespace cv;
 using namespace std;
+using namespace cv;
+
+int main(){
+
+    VideoCapture vcap(0);
+      if(!vcap.isOpened()){
+             cout << "Error opening video stream or file" << endl;
+             return -1;
+      }
+
+   int frame_width=   vcap.get(CV_CAP_PROP_FRAME_WIDTH);
+   int frame_height=   vcap.get(CV_CAP_PROP_FRAME_HEIGHT);
+   VideoWriter video("out.avi",CV_FOURCC('M','J','P','G'),10, Size(frame_width,frame_height),true);
+
+   for(;;){
+       Mat image;
+       vcap >> image;
+
+       imshow( "Frame", image );
+
+       vector<Point2f> corners;
+       Size chessSize(CBOARD_COL,CBOARD_ROW);
+
+       std::vector<Point2f> fourCorners;
+
+       bool patternfound = findChessboardCorners(image, chessSize, corners, CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE + CALIB_CB_FAST_CHECK);
+       if(patternfound)
+       {
+           Mat gray;
+           cvtColor(image,gray,CV_BGR2GRAY);
+           cornerSubPix(gray,corners,Size(11,11),Size(-1,-1),TermCriteria(CV_TERMCRIT_EPS+ CV_TERMCRIT_ITER,30,0.1));
 
 
+       // 0 4 15 19...
+       fourCorners.push_back(corners[0]);
+       fourCorners.push_back(corners[CBOARD_ROW]);
+       fourCorners.push_back(corners[CBOARD_ROW*(CBOARD_COL-1)-1]);
+       fourCorners.push_back(corners[CBOARD_COL*CBOARD_ROW-1]);
 
-int main(int argc, char**argv)
-{
-    if(argc!=2)
-    {
-            cout << "Usage: chessboard imageToLoad" << endl;
-            return -1;
+       for(int i = 0; i<4; i++)
+       {
+           circle(image, fourCorners[i], 1, cv::Scalar( 50. ), -1 );
+           std::string display_string;
+           std::stringstream out;
+           out << i;
+           display_string = out.str();
+
+           //Add numbering to the four points discovered.
+           cv::putText( image, display_string, fourCorners[i], CV_FONT_HERSHEY_COMPLEX, 1,cv::Scalar(255.), 1, 1);
+       }
+
+       cout << corners << endl;
+
+       namedWindow("Image2");
+       imshow("Image2",image);
+        }
+
+       video.write(image);
+       char c = (char)waitKey(33);
+       if( c == 27 ) break;
     }
-std::clock_t start;
-start = std::clock();
-
-    Mat image;
-    image = imread(argv[1],CV_LOAD_IMAGE_COLOR);
-
-    if(!image.data)
-    {
-            cout << "Could not open or find the image" << endl;
-            return -1;
-    }
-
-
-    vector<Point2f> corners;
-    Size chessSize(CBOARD_COL,CBOARD_ROW);
-
-    std::vector<Point2f> fourCorners;
-
-    bool patternfound = findChessboardCorners(image, chessSize, corners, CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE + CALIB_CB_FAST_CHECK);
-    if(patternfound)
-    {
-        Mat gray;
-        cvtColor(image,gray,CV_BGR2GRAY);
-        cornerSubPix(gray,corners,Size(11,11),Size(-1,-1),TermCriteria(CV_TERMCRIT_EPS+ CV_TERMCRIT_ITER,30,0.1));
-    }
-    // 0 4 15 19...
-    fourCorners.push_back(corners[0]);
-    fourCorners.push_back(corners[CBOARD_ROW]);
-    fourCorners.push_back(corners[CBOARD_ROW*(CBOARD_COL-1)-1]);
-    fourCorners.push_back(corners[CBOARD_COL*CBOARD_ROW-1]);
-
-    for(int i = 0; i<4; i++)
-    {
-        circle(image, fourCorners[i], 1, cv::Scalar( 50. ), -1 );
-    }
-    cout << corners << endl;
-/*
-    namedWindow("Image2");
-    imshow("Image2",image);
-    waitKey(0);
-*/
-    //IBVS PORTION
-    IBVS ibvs = IBVS();
-    ibvs.update_uv(fourCorners);
-    ibvs.disp_uv();
-    ibvs.update_Le(1);
-    ibvs.display_Le();
-    ibvs.MP_psinv_Le();
-
-
-    std::cout << '\n' << "Time: " << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
-
-
-    return 0;
-	
+  return 0;
 }
